fdf973c0dfb573ae8bb05807675523499b971820
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2023-06-29 20:02:26 -0400

    versão 0.4 5nights 29/06/23

--- modulo03/projeto-5noites/script/script.js
+++ modulo03/projeto-5noites/script/script.js
@@ -1,24 +1,25 @@
 var muteIcon = document.getElementById('icon-som');
-var audio = new Howl({
-  src: ['audio/FiveNightsAtFreddysSong-Final.mp3']
-});
+var audio = document.getElementById('music')
 var playIcon = document.getElementById('icon-play');
 var num = 0;
 playIcon.addEventListener('click', function() {
   if (num%2==0) {
-    audio.play();
-    playIcon.src
-    num++;
+      audio.play();
+      playIcon.src = "imagens/pause-pq.png";
+      num++;
   } else {
     audio.pause();
-    num++
+    playIcon.src = "imagens/play-pq.png";
+    num++;
   }
 }
 )
 muteIcon.addEventListener('click', function() {
-  if (audio.play()) {
-    audio.muted = true;
+  if (audio.muted) {
+    audio.muted = false;
+    muteIcon.src = "imagens/music-player-pq.png";
   } else {
     audio.muted = true;
+    muteIcon.src = "imagens/mute-pq.png";
   }
 });
