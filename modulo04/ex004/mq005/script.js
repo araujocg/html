2f1b298616826cd3ffa5d04e9cec12a55e851740
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2023-09-01 17:56:15 -0400

    ex004 mq005 01/09/23

--- modulo04/ex004/mq005/script.js
+++ modulo04/ex004/mq005/script.js
@@ -5,3 +5,11 @@ function clickMenu(){
         menu.style.display = 'none'
     }
 }
+
+function changedSize(){
+    if (window.innerWidth >= 768){
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+}
